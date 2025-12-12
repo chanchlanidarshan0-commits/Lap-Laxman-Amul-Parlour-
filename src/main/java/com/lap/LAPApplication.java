f362ae0ed85ca8b65d227a11200a2cbd56807eb5
chanchlanidarshan0-commits <chanchlanidarshan0@gmail.com>
2025-12-12 20:02:57 +0530

    LAPApplication.java

---
+++ src/main/java/com/lap/LAPApplication.java
@@ -0,0 +1,11 @@
+package com.lap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LAPApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LAPApplication.class, args);
+    }
+}
